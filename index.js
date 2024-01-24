4a6e355308908990ae8adaeecd053822974dcae5
Rohit Sahoo <77395879+rohitsahoo11@users.noreply.github.com>
2024-01-24 19:39:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var pass = document.getElementById('password')
+var msg = document.getElementById('message')
+var strength = document.getElementById('strength')
+
+pass.addEventListener('input', ()=>{
+    if(pass.value.length > 0){
+        msg.style.display = "block"
+    }
+    else{
+        msg.style.display = "none"
+    }
+    if(pass.value.length < 4){
+        strength.innerHTML = "weak"
+        pass.style.borderColor = 'red'
+        msg.style.color = 'red'
+    }
+    else if(pass.value.length >= 4 && pass.value.length<=8){
+        strength.innerHTML = "medium"
+        pass.style.borderColor = 'yellow'
+        msg.style.color = 'yellow'
+    }
+    else if(pass.value.length >= 8){
+        strength.innerHTML = "strong"
+        pass.style.borderColor = '#26d730'
+        msg.style.color = '#26d730'
+    }
+})  
